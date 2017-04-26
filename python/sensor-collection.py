8d046bea78d89b9fa0e47e2e2a611026532d6f4a
lucent.peng <lucent.peng@deltaww.com>
2017-04-26 16:33:51 +0800

    [Sensor]Modify collection function
    1. Add read light sensor in daemon thread
    2. Re-structure code
     Changes to be committed:
    	modified:   sensor-collection.py

--- python/sensor-collection.py
+++ python/sensor-collection.py
@@ -5,30 +5,41 @@ import os
 import time;
 import socket;
 import sys;
+from threading import Thread
 
 """=====Input Variable Start======
 If test mode is on, use hard code input
 In the other side, use argv input"""
-test_mode = 1
-t_ref = 0
+t_ref = 0	    #There are many IPCS on the test chamber, PC need send reference time related to 1st test device.
+threadshold = 1100   #Data collection Critera , it is pressure value
 """=====Input Variable End======"""
-
-#Flag to show sensor collection status
-collect = 0
 
 #Output Data Pattern
 Ppat = re.compile('^P[0-9]+\.[0-9]')
 Apat = re.compile('[PCVTHD][0-9.]+')
 
-#Read ipcs host name, the host name should be serial number of board
-hostname = socket.gethostname()
-print hostname
-print 'ipcs 3.0 sensor test!'
 
-filenames = [   str(hostname)+'.p.csv', str(hostname)+'.t.csv', str(hostname)+'.h.csv',
-		str(hostname)+'.d.csv', str(hostname)+'.v.csv', str(hostname)+'.c.csv']
-fd = [ open(filename, 'w') for filename in filenames ]
-num = [ 'P', 'T', 'H', 'D', 'V', 'C']
+num = [ 'P', 'T', 'H', 'D', 'V', 'C', 'L']
+
+def open_file() :
+    fd = []
+    #Read ipcs host name, the host name should be serial number of board
+    hostname = str(socket.gethostname())
+    for n in num :
+	filename = hostname + '.' + n.lower() + '.csv'
+	fd.append(open(filename, 'w'))
+    return fd
+
+def write_to_file( fd, sensor, value, timestamp ) :
+	idx = num.index( sensor )
+	print("COLLECTION<==TYPE({0:s}),VALUE({1:.1f}),TIME({2:d})".format(sensor, float(value),int(timestamp)))
+	fd[idx].write( str(value) + ',' + str(int(timestamp)) + '\n')
+	fd[idx].flush()
+
+def close_file( fd ) :
+    print 'Close sensor collection data.'
+    for index in range(len(fd)) :
+	fd[index].close()
 
 def open_serial_port() : #Open ttyS1 UART port.
     port = serial.Serial(
@@ -43,58 +54,73 @@ def open_serial_port() : #Open ttyS1 UART port.
     return port
 
 
+
+def readLightSensor( fd ):
+    while True:
+	adc = os.popen('cat /sys/bus/iio/devices/iio:device0/in_voltage0_raw').read()
+	lux = abs(int(adc)) * 101 >> 8
+	t_sensor = time.time() - t_start + t_ref
+	write_to_file( fd, 'L', lux, t_sensor)
+	time.sleep(5) #update lux per 5 seconds
+
+
+def do_collection( port, fd ) :
+    collect = 0	    #Flag to identify collection start ot stop
+    while os.path.isfile("run"):
+	try:
+	    raw = port.read(1)           # Wait forever for anything
+	    data_left = port.inWaiting()  # Get the number of characters ready to be read
+	    raw += port.read(data_left) # Do the read and combine it with the first character
+	    print raw
+	except :
+	    print "Oops! Try again..."
+	    port = open_serial_port()
+
+	t_sensor = time.time() - t_start + t_ref
+	#    print "elspase time is %d" %(t_sensor-t_start+t_ref)
+
+	if Ppat.search(raw):
+	    value = filter(lambda form: form in '0123456789.', Ppat.findall(raw)[0])
+	    #	print value
+	    if (float(value) < threadshold):
+		print 'Start to collect sensor data.'
+		collect = 1
+		write_to_file( fd, 'P', value, t_sensor)
+	    else:
+		print 'Stop to collect sensor data'
+		collect = 0
+
+	if (collect == 1):
+	    it = re.finditer(Apat, raw)
+	    for match in it:
+		print "'{g}' was found, {s}".format(g=match.group(), s=match.span())
+		g = match.group()
+		sensor_type = g[0]
+		print sensor_type
+
+		value = filter(lambda form: form in '0123456789.', g)
+
+		if( sensor_type != 'P' ) :
+		    write_to_file( fd, sensor_type, value, t_sensor)
+
+###################################
+print 'ipcs 3.0 sensor test start!'
+
 t_start = time.time()
 print "start time is %d" %(t_start)
 
-
 port = open_serial_port()
+fd = open_file()
 
-while os.path.isfile("run"):
-    try:
-	raw = port.read(1)           # Wait forever for anything
-	data_left = port.inWaiting()  # Get the number of characters ready to be read
-	raw += port.read(data_left) # Do the read and combine it with the first character
-	print raw
-    except :
-	print "Oops! Try again..."
-	port = open_serial_port()
+thread_light = Thread(target=readLightSensor, args=(fd,))
+thread_light.setDaemon(True)
+thread_light.start()
 
-    t_sensor = time.time() - t_start + t_ref
-#    print "elspase time is %d" %(t_sensor-t_start+t_ref)
+do_collection( port, fd );
 
-    if Ppat.search(raw):
-	value = filter(lambda form: form in '0123456789.', Ppat.findall(raw)[0])
-#	print value
-	if (float(value) < 1100.0):
-	    print 'Pressure value <= critera'
-	    print 'Start to collect sensor data.'
-	    collect = 1
-	    fd[0].write( str(value) + ',' + str(int(t_sensor)) + '\n')
-	    fd[0].flush()
-	else:
-	    print 'Pressure value > critera'
-	    print 'Stop to collect sensor data'
-	    collect = 0
-
-    if (collect == 1):
-	it = re.finditer(Apat, raw)
-	for match in it:
-	    print "'{g}' was found, {s}".format(g=match.group(), s=match.span())
-	    g = match.group()
-	    sensor_type = g[0]
-	    print sensor_type
-
-	    value = filter(lambda form: form in '0123456789.', g)
-
-	    if( sensor_type != 'P' ) :
-		print("COLLECTION<==TYPE({0:s}),VALUE({1:.1f}),TIME({2:d})".format(sensor_type, float(value),int(t_sensor)))
-		fd[ num.index(sensor_type) ].write( str(value) + ',' + str(int(t_sensor)) + '\n' )
-		fd[ num.index(sensor_type) ].flush()
-		
-
-print 'Close sensor collection data.'
-for index in range(len(fd)):
-    fd[index].close()
-
+close_file(fd)
 print 'Close ttyS1 UART port.'
 port.close()
+
+print 'ipcs 3.0 sensor test finish!'
+###################################
